428de1a4f65cce8f27f836f87935a51b7d01fab8
Vasily Pisar <vapisar@microsoft.com>
2021-05-28 03:56:57 -0700

    Checking claims and app roles

--- MultiCdnApi/src/Functions/UserFunctions.cs
+++ MultiCdnApi/src/Functions/UserFunctions.cs
@@ -4,16 +4,14 @@
 namespace MultiCdnApi
 {
     using System.Linq;
-    using System.Net;
-    using System.Net.Http;
     using System.Security.Claims;
     using System.Text;
-    using System.Web.Http;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Azure.WebJobs;
     using Microsoft.Azure.WebJobs.Extensions.Http;
     using Microsoft.Extensions.Logging;
+    using Microsoft.Identity.Web.Resource;
 
     public class UserFunctions
     {
@@ -35,8 +33,45 @@ namespace MultiCdnApi
         {
             var httpContext = req.HttpContext;
             var httpContextUser = httpContext.User;
-            return new JsonResult(httpContextUser);
+            var result = new StringBuilder();
+
+            result.Append("Claims: {\n");
+            foreach (var claim in httpContextUser.Claims)
+            {
+                result.Append("claim: {").Append(SerializeClaim(claim)).Append("};\n");
+            }
+            result.Append("}\n");
+
+            result.Append("Identities: {\n");
+            foreach (var claimsIdentity in httpContextUser.Identities)
+            {
+                result.Append("identity: {");
+                result.Append(string.Join(",", claimsIdentity.Claims)).Append(";");
+                result.Append(string.Join(",",
+                    claimsIdentity.Label, claimsIdentity.Name, claimsIdentity.AuthenticationType,
+                    claimsIdentity.NameClaimType, claimsIdentity.RoleClaimType)).Append(";");
+                result.Append("};\n");
+            }
+            result.Append("}\n");
+
+            result.Append("Identity: {\n");
+            result.Append(httpContextUser.Identity);
+            result.Append("}\n");
+
+            return new JsonResult(result.ToString());
         }
+
+        [FunctionName("GetClaims")]
+        public IActionResult GetClaims(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "user/claims")]
+            HttpRequest req,
+            ILogger log)
+        {
+            var httpContext = req.HttpContext;
+            var httpContextUser = httpContext.User;
+            return new JsonResult(string.Join("; ", httpContextUser.Claims.Select(SerializeClaim)));
+        }
+
 
         [FunctionName("GetGroups")]
         public IActionResult GetGroups(
@@ -60,6 +95,23 @@ namespace MultiCdnApi
         {
             var roleClaim = ClaimsPrincipal.Current.FindFirst("roles");
             return new JsonResult(roleClaim);
+        }
+
+        [FunctionName("GetApiScopes")]
+        public IActionResult GetApiScopes(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "user/roles")]
+            HttpRequest req,
+            ILogger log)
+        {
+            req.HttpContext.ValidateAppRole("");
+            return new JsonResult("OK");
+        }
+
+        private static string SerializeClaim(Claim claim)
+        {
+            var claimProperties = string.Join(";",
+                claim.Issuer, claim.OriginalIssuer, claim.Properties, claim.Subject, claim.ValueType);
+            return claim + " (" + claimProperties + ")";
         }
     }
 }
